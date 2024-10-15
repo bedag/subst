6b9987f364a1c697294c5bb7318fa43c605a72c2
Adrian Berger <adrian.berger@bedag.ch>
2024-10-15 14:16:21 +0200

    Revert "chore: Remove GOMAXPROCS & GOMEMLIMIT"
    This reverts commit 49785f8403ea238e54ca72df96ed78223901aa62.

--- subst/main.go
+++ subst/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"log/slog"
+
+	"github.com/KimMachineGun/automemlimit/memlimit"
 	"github.com/bedag/subst/subst/cmd"
+
+	_ "go.uber.org/automaxprocs"
 )
 
 func main() {
+	memlimit.SetGoMemLimitWithOpts(
+		memlimit.WithRatio(0.9),
+		memlimit.WithProvider(
+			memlimit.ApplyFallback(
+				memlimit.FromCgroup,
+				memlimit.FromSystem,
+			),
+		),
+		memlimit.WithLogger(slog.Default()),
+	)
 	cmd.Execute()
 }
